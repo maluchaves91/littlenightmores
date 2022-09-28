211af6b8d4eafff0adedfa7932c8ff9457488016
Maria de Lourdes Carvalho Chaves <95364349+maluchaves91@users.noreply.github.com>
2022-09-28 15:21:50 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function voltarTema(){
+    document.getElementById("body").style.backgroundColor="rgb(87, 92, 92)";
+
+    document.getElementById("body").style.color="black";
+
+    document.getElementById("tema").src = "2.png"
+
+}
+
+
+function mudarTema()
+{
+    document.getElementById("body").style.backgroundColor="black";
+
+    document.getElementById("body").style.color="white";
+
+    document.getElementById("tema").src = "3.png"
+
+}
+
+function changeImageOver()
+{
+    document.getElementById("little").src ="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTin0yPpe5JtCIR2OCeqiUBnHLGWmHtMw2KSg&usqp=CAU";
+}
+
+function changeImageLeave()
+{
+    document.getElementById("little").src ="https://th.bing.com/th/id/OIP.iV_sirb7nqbIRvtRn46BrAHaEK?w=300&h=180&c=7&r=0&o=5&dpr=1.25&pid=1.7";
+}
+
